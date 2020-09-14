aad2c2d0cc300d778314bb2aa20499a42b6f9217
stefanwert <stefan.petrovic2345@gmail.com>
2020-09-14 22:55:06 +0200

    Povezan datePicker

--- WebProject1/static/app/guest/apartmentDetails.js
+++ WebProject1/static/app/guest/apartmentDetails.js
@@ -1,3 +1,11 @@
+function checkDate(date1,date2){
+	
+	date2 = new Date(date2);
+	return  date1.getDate() == date2.getDate() && 
+			date1.getMonth() == date2.getMonth() &&
+			date1.getYear() == date2.getYear();
+}
+
 Vue.component("apartment-detail", {
 	data: function () {
 		    return {
@@ -5,13 +13,25 @@ Vue.component("apartment-detail", {
 		      apartment: {},
 		      pictures:[],
 		      apartmentId:-1,
-		      selectedDate:[]
+		      selectedDate:[],
+		      disabledDates:{
+		    	  customPredictor:(date)=> {
+		    		  var a=0;
+		    	      for(var date2 of this.apartment.availableDates){
+		    	    	  
+		    	    	  if(checkDate(date,date2)){
+		    	    		 return false;
+		    	    	  }
+		    	      }
+		    	      return true;
+		    	  }
+		      }
 		    }
 	},
 	template: ` 
 <div>
 		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-			  
+			  <link href="">
 			  <div class="carousel-inner">
 			  		<div  class="carousel-item active">
 				      <img id="pic" class="d-block w-100" :src=address(apartment.pictures[0]) alt="First slide">
@@ -32,7 +52,9 @@ Vue.component("apartment-detail", {
 		<table>
 			<tr>
 				<td>Slobodni dani:</td>
-				<td>
+				<td id="nista" style="color:black;">
+					  <vuejs-datepicker :disabled-dates=disabledDates></vuejs-datepicker>
+					
 					<div class="d-flex flex-row">
 						<select v-model="selectedLocation">
 							<option v-for="d in apartment.availableDates"   v-bind:value="d">{{d}}</option>
@@ -54,6 +76,14 @@ Vue.component("apartment-detail", {
 	methods : {
 		address: function (pic){
 			return "slike/"+pic;
+		},
+		disabledDates:function(date){
+			for(i=0;i<this.apartment.availableDates;i++){
+				if(date.getDate()==this.apartment.availableDates[i]){
+					return true;
+				}
+			}
+			return false;
 		}
 	},
 	mounted () {
@@ -68,5 +98,32 @@ Vue.component("apartment-detail", {
 			this.pictures.shift(); 
 			console.log(this.apartment);
 		});
-    }
+        this.disabledDates.customPredictor=function(date) {
+	    	      // disables the date if it is a multiple of 5
+	    	      
+	    	    	  
+	    	    }
+	    	  
+    },
+    methods : {
+		address: function (pic){
+			return "slike/"+pic;
+		}
+	},
+	mounted () {
+        var id=router.currentRoute.params.apId;
+        axios
+        .get("/Apartments?id="+id)
+		.then(response => {
+			this.apartment = response.data;
+			for(i=0;i<this.apartment.pictures.length;i++){
+				this.pictures[i]=this.apartment.pictures[i];
+			}
+			this.pictures.shift(); 
+			console.log(this.apartment);
+		});
+    },
+    components: {
+      	vuejsDatepicker
+      }
 });
